27112a50e3057c99b560cd212551f82b1d87a378
Art Karasev <as.karasyov@gmail.com>
2023-07-26 20:41:59 +1200

    initial components

---
+++ client/src/vite.config.js
@@ -0,0 +1,67 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import autoprefixer from "autoprefixer";
+import * as path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+
+  resolve: {
+      alias: {
+          '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons/icons'),
+      }
+  },
+
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return '[name][extname]'
+          } else if (
+            assetInfo.name.match(/(\.(woff2?|eot|ttf|otf)|font\.svg)(\?.*)?$/)
+          ) {
+            return 'fonts/[name][extname]'
+          } else if (assetInfo.name.match(/\.(jpg|png|svg)$/)) {
+            return 'images/[name][extname]'
+          }
+
+          return 'js/[name][extname]'
+        }
+      }
+    }
+  },
+
+  plugins: [
+
+      laravel({
+          input: [
+              'src/bootstrap-icons.scss',
+          ],
+          refresh: true,
+          // buildDirectory: '',
+      }),
+
+      viteStaticCopy({
+        targets: [
+          {
+            src: './node_modules/bootstrap-icons/icons/*',
+            dest: '../dist/bootstrap-icons',
+          },
+        ],
+      })
+  ],
+
+  css: {
+      postcss: {
+          plugins: [
+              autoprefixer,
+          ],
+      }
+  },
+
+});
